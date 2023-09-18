11cdd14616761797e0afc117b033042b0247b3bd
Simon Diradourian <simond@kickmaker.net>
2023-09-18 17:49:19 +0200

    on avance

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -3,12 +3,24 @@
 // pass! Make the test fail!
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+#[derive(PartialEq, Eq)]
+#[derive(Debug)]
+pub struct Test {
+    a : u32
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let a = Test {
+            a: 2
+        };
+        let b = Test {
+            a: 2
+        };
+        assert_eq!(b, a);
     }
 }
